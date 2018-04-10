d233c8d33368e48bfe05c111b0ed26867476e7df
Antonio21MP <antonio.mperdomo21@gmail.com>
2018-04-09 20:58:00 -0600

    models practice

--- server/server.js
+++ server/server.js
@@ -5,27 +5,53 @@ mongoose.connect('mongodb://localhost:27017/TodoDB');
 
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        require: true,
+        minlength: 1,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: false
     }
+});
+
+var User = mongoose.model('Users', {
+    email: { 
+        type: String,
+        require: true,
+        minlength: 1,
+        trim: true
+    }
+})
+
+var newUser = new User({
+    email: 'antonio.mperdomo21@gmail.com'
+});
+
+newUser.save().then((doc) => {
+    console.log(JSON.stringify(doc, undefined, 2));
+}, (err) => {
+    console.log('Unable to save user');
 });
 /*
 var newTodo = new Todo({
     text: 'Cook dinner'
 });
 */
+/*
 var newTodo = new Todo({
     text: 'Cook lunch',
     completed: 'false',
     completedAt: 0
 });
 newTodo.save().then((doc) => {
-    console.log('Save todo. ',doc);
+    console.log(JSON.stringify(doc, undefined, 2));
 }, (err) => {
     console.log('Unable to save todo. ', err);
 });
+*/
