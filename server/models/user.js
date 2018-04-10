ffbfbd6eee29a005b257f5dc47498b1d62b711f2
Antonio21MP <antonio.mperdomo21@gmail.com>
2018-04-10 00:24:49 -0600

    Test POST /todo

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var User = mongoose.model('Users', {
     email: { 
         type: String,
-        require: true,
+        required: true,
         minlength: 1,
         trim: true
     }
