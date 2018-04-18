92367afc075b63185e58b3127db9187f4848c039
Antonio21MP <antonio.mperdomo21@gmail.com>
2018-04-18 17:50:37 -0600

    Setup separate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -8,7 +10,7 @@ const {ObjectID} = require('mongodb');
 const app = express();
 const _ = require('lodash');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
