951ec8c4e849bc0e073cbd1808120b071695f0b8
Antonio21MP <antonio.mperdomo21@gmail.com>
2018-04-20 23:10:59 -0600

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -108,6 +108,17 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  User.findByCrendentials(body.email, body.password).then((user) => {
+    user.generateAuthToken().then((token) =>{
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((err) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started up at port: ${port}`);
 });
